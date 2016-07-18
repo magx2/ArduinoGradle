07d8f84a32fb9f2b9facf79dcb9576ed972af28b
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-18 23:54:07 +0200

    new gardle plugin and creating mocks for tasks and plugin

---
+++ plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/ArduinoPlugin.groovy
@@ -0,0 +1,11 @@
+package com.github.magx2.gradle.arduino
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ArduinoPlugin implements Plugin<Project> {
+	@Override
+	void apply(Project target) {
+
+	}
+}
---
+++ plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/tasks/InstallBoardsArduinoTask.groovy
@@ -0,0 +1,6 @@
+package com.github.magx2.gradle.arduino.tasks
+
+import org.gradle.api.DefaultTask
+
+class InstallBoardsArduinoTask extends DefaultTask {
+}
