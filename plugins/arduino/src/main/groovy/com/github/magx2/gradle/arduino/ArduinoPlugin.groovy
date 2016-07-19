99b6502266c0b864409600b18b100ca6b7bde318
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-19 21:11:44 +0200

    Adding cerated tasks

--- plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/ArduinoPlugin.groovy
+++ plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/ArduinoPlugin.groovy
@@ -1,11 +1,17 @@
 package com.github.magx2.gradle.arduino
 
+import com.github.magx2.gradle.arduino.tasks.CompileArduinoTask
+import com.github.magx2.gradle.arduino.tasks.PrecompileArduinoTask
+import com.github.magx2.gradle.arduino.tasks.UploadArduinoTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class ArduinoPlugin implements Plugin<Project> {
 	@Override
-	void apply(Project target) {
+	void apply(Project project) {
+		project.tasks.create("precompileArduino", PrecompileArduinoTask)
 
+		project.tasks.create("uploadArduino", UploadArduinoTask)
+		project.tasks.create("compileArduino", CompileArduinoTask)
 	}
 }
