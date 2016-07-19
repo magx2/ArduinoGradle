3e1c15d3a247caa0d2e09f559cc74112c03bded0
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-19 22:16:14 +0200

    Adding group for task and fixing cleaning tasks

--- plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/ArduinoPlugin.groovy
+++ plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/ArduinoPlugin.groovy
@@ -8,15 +8,24 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class ArduinoPlugin implements Plugin<Project> {
+	private static final String TASKS_GROUP = 'arduino'
+
 	@Override
 	void apply(Project project) {
 		final cleanArduino = project.tasks.create("cleanArduino", CleanArduinoTask)
-		project.tasks['clean']?.dependsOn cleanArduino
+		cleanArduino.group = TASKS_GROUP
+		project.getTasksByName('clean', false)?.each { clean ->
+			clean.dependsOn cleanArduino
+		}
 
 		final precompileArduino = project.tasks.create("precompileArduino", PrecompileArduinoTask)
+		precompileArduino.group = TASKS_GROUP
 
 		final uploadArduino = project.tasks.create("uploadArduino", UploadArduinoTask)
 		final compileArduino = project.tasks.create("compileArduino", CompileArduinoTask)
+
+		uploadArduino.group = TASKS_GROUP
+		compileArduino.group = TASKS_GROUP
 
 		uploadArduino.dependsOn precompileArduino
 		compileArduino.dependsOn precompileArduino
