e96fb92e443665a77d7a6f667eb055c12dd9ce98
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-19 18:30:11 +0200

    Adding NoOpTemplateEngine that does nothing to templates

--- plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/tasks/PrecompileArduinoTask.groovy
+++ plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/tasks/PrecompileArduinoTask.groovy
@@ -3,6 +3,7 @@ package com.github.magx2.gradle.arduino.tasks
 import com.github.magx2.gradle.FileUtils
 import com.github.magx2.gradle.arduino.tasks.templateengines.FreemarkerTemplateEngine
 import com.github.magx2.gradle.arduino.tasks.templateengines.MoustacheTemplateEngine
+import com.github.magx2.gradle.arduino.tasks.templateengines.NoOpTemplateEngine
 import com.github.magx2.gradle.arduino.tasks.templateengines.TemplateEngine
 import groovy.io.FileType
 import org.gradle.api.DefaultTask
@@ -12,12 +13,13 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
 class PrecompileArduinoTask extends DefaultTask {
+	public static final TemplateEngine NO_OP_TEMPLATE_ENGINE = new NoOpTemplateEngine()
 	public static final TemplateEngine MOUSTACHE_TEMPLATE_ENGINE = new MoustacheTemplateEngine()
 	public static final TemplateEngine FREEMARKER_TEMPLATE_ENGINE = new FreemarkerTemplateEngine()
 
 	@InputDirectory File srcDir = new File("src/main/arduino")
 	@OutputDirectory File precompiledDir = new File("$project.buildDir/arduiono/precompiled")
-	@Input TemplateEngine templateEngine = MOUSTACHE_TEMPLATE_ENGINE
+	@Input TemplateEngine templateEngine = NO_OP_TEMPLATE_ENGINE
 	@Input Map<String, String> context = [:]
 
 	@TaskAction
---
+++ plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/tasks/templateengines/NoOpTemplateEngine.groovy
@@ -0,0 +1,5 @@
+package com.github.magx2.gradle.arduino.tasks.templateengines
+
+final class NoOpTemplateEngine implements TemplateEngine {
+	@Override String precompile(String text, Map<String, String> context) { text }
+}
