2411979b5fa2028f74febf5d702e475c9d8e3702
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-21 13:53:25 +0200

    Implementacja NanoPbCompileTask.groovy

--- plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/NanoPbPlugin.groovy
+++ plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/NanoPbPlugin.groovy
@@ -1,6 +1,7 @@
 package com.github.magx2.gradle.nanopb
 
 import com.github.magx2.gradle.nanopb.tasks.CleanNanoPbTask
+import com.github.magx2.gradle.nanopb.tasks.NanoPbCompileTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -14,5 +15,8 @@ class NanoPbPlugin implements Plugin<Project> {
 		project.getTasksByName('clean', false)?.each { clean ->
 			clean.dependsOn cleanArduino
 		}
+
+		final compileNanoPb = project.tasks.create("compileNanoPb", NanoPbCompileTask)
+		compileNanoPb.group = TASKS_GROUP
 	}
 }
--- plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/tasks/NanoPbCompileTask.groovy
+++ plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/tasks/NanoPbCompileTask.groovy
@@ -1,11 +1,58 @@
 package com.github.magx2.gradle.nanopb.tasks
 
+import com.github.magx2.gradle.CommandUtils
+import com.github.magx2.gradle.utils.exceptions.NotSetReferenceException
+import groovy.io.FileType
+import groovy.transform.CompileStatic
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
 class NanoPbCompileTask extends DefaultTask {
-	@TaskAction
-	def compileNanoPb() {
+	@OutputDirectory File compileDir
+	@InputDirectory File nanopbBin = project.nanopbBin ? project.nanopbBin as File : null
+	@Input @Optional List<File> protos
+	File defaultProtosDir = "src/main/proto" as File
 
+	NanoPbCompileTask() {
+		final mainDir = project.tasks['cleanNanoPb']?.mainDir
+		if(mainDir) {
+			compileDir = new File((File) mainDir, "compiled")
+		}
+	}
+
+	@TaskAction
+	@CompileStatic
+	def compileNanoPb() {
+		if (!nanopbBin) throw new NotSetReferenceException("nanopbBin")
+		if (!compileDir) throw new NotSetReferenceException("compileDir")
+
+		if(!protos) {
+			addProtosFromDefaultPath()
+		}
+		if (!protos) throw new NotSetReferenceException("protos")
+
+
+		final cmd = []
+		cmd <<"$nanopbBin.absolutePath/protoc"
+		cmd << "--nanopb_out=$compileDir.absolutePath"
+		cmd << protos.join(" ")
+
+		CommandUtils.execute(cmd as String[])
+	}
+
+	@CompileStatic
+	private void addProtosFromDefaultPath() {
+		logger.info("Protos list is empty, searching for protos in \"$defaultProtosDir.absolutePath\"")
+		if (!defaultProtosDir) throw new NotSetReferenceException("defaultProtosDir")
+		defaultProtosDir.eachFileRecurse(FileType.FILES) { file ->
+			if (file.name.endsWith(".proto")) {
+				logger.debug("Adding file \"$file.absolutePath\"")
+				protos << file
+			}
+		}
 	}
 }
