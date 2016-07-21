91573083266b2b2af65566febb824680582ce5bb
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-21 14:42:44 +0200

    For CompileArduinoTask.groovy preserveTempFile should be true

--- plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/tasks/CompileArduinoTask.groovy
+++ plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/tasks/CompileArduinoTask.groovy
@@ -2,4 +2,9 @@ package com.github.magx2.gradle.arduino.tasks
 
 class CompileArduinoTask extends ArduinoTask {
 	@Override protected String option() { "--verify" }
+
+	CompileArduinoTask() {
+		super()
+		preserveTempFiles = true
+	}
 }
