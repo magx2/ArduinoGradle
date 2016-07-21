7dae214e4dd980cbe2207558a1c9fc24ccefeea5
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-21 13:26:29 +0200

    Implementing clean task

--- plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/NanoPbPlugin.groovy
+++ plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/NanoPbPlugin.groovy
@@ -1,11 +1,18 @@
 package com.github.magx2.gradle.nanopb
 
+import com.github.magx2.gradle.nanopb.tasks.NanoPbCleanTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class NanoPbPlugin implements Plugin<Project> {
-	@Override
-	void apply(Project target) {
+	private static final String TASKS_GROUP = 'nanopb'
 
+	@Override
+	void apply(Project project) {
+		final cleanArduino = project.tasks.create("cleanNanoPb", NanoPbCleanTask)
+		cleanArduino.group = TASKS_GROUP
+		project.getTasksByName('clean', false)?.each { clean ->
+			clean.dependsOn cleanArduino
+		}
 	}
 }
--- plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/tasks/NanoPbCleanTask.groovy
+++ plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/tasks/NanoPbCleanTask.groovy
@@ -1,11 +1,16 @@
 package com.github.magx2.gradle.nanopb.tasks
 
+import groovy.transform.CompileStatic
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.TaskAction
 
 class NanoPbCleanTask extends DefaultTask {
+	@InputDirectory File mainDir = "$project.buildDir/nanopb" as File
+
+	@CompileStatic
 	@TaskAction
 	def clean() {
-
+		mainDir.deleteDir()
 	}
 }
