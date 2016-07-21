e2fd94b7d72e7f0d8bdb9ec93d2de8cfe7834008
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-21 17:39:44 +0200

    Fixd path to clean arduino

--- plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/tasks/CleanArduinoTask.groovy
+++ plugins/arduino/src/main/groovy/com/github/magx2/gradle/arduino/tasks/CleanArduinoTask.groovy
@@ -1,12 +1,10 @@
 package com.github.magx2.gradle.arduino.tasks
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.TaskAction
 
 class CleanArduinoTask extends DefaultTask {
-
-	@InputDirectory File arduinoMainBuildPath = "build/arduino" as File
+	File arduinoMainBuildPath = "$project.buildDir/arduino" as File
 
 	@TaskAction
 	void clean() {
