730e90be18fa2611feec03b21d353a2beb68577b
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-21 13:23:27 +0200

    Mock of project and tasks

---
+++ plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/NanoPbPlugin.groovy
@@ -0,0 +1,11 @@
+package com.github.magx2.gradle.nanopb
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class NanoPbPlugin implements Plugin<Project> {
+	@Override
+	void apply(Project target) {
+
+	}
+}
---
+++ plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/tasks/NanoPbCompileTask.groovy
@@ -0,0 +1,11 @@
+package com.github.magx2.gradle.nanopb.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class NanoPbCompileTask extends DefaultTask {
+	@TaskAction
+	def compileNanoPb() {
+
+	}
+}
