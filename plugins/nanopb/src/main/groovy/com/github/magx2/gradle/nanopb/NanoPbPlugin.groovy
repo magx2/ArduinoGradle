cec7438b43bc20b6744f448333dd1aaddc2efff0
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-07-21 13:54:47 +0200

    @CompileStatic

--- plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/NanoPbPlugin.groovy
+++ plugins/nanopb/src/main/groovy/com/github/magx2/gradle/nanopb/NanoPbPlugin.groovy
@@ -2,6 +2,7 @@ package com.github.magx2.gradle.nanopb
 
 import com.github.magx2.gradle.nanopb.tasks.CleanNanoPbTask
 import com.github.magx2.gradle.nanopb.tasks.CompileNanoPbTask
+import groovy.transform.CompileStatic
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -9,16 +10,19 @@ class NanoPbPlugin implements Plugin<Project> {
 	private static final String TASKS_GROUP = 'nanopb'
 
 	@Override
+	@CompileStatic
 	void apply(Project project) {
 		cleanNanoPb(project)
 		compileNanoPb(project)
 	}
 
+	@CompileStatic
 	private static void compileNanoPb(Project project) {
 		final compileNanoPb = project.tasks.create("compileNanoPb", CompileNanoPbTask)
 		compileNanoPb.group = TASKS_GROUP
 	}
 
+	@CompileStatic
 	private static void cleanNanoPb(Project project) {
 		final cleanArduino = project.tasks.create("cleanNanoPb", CleanNanoPbTask)
 		cleanArduino.group = TASKS_GROUP
